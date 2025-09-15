7c36693a3336eeaece46103843acc5b0010512a7
Mats <mat.eidelman@gmail.com>
2025-09-15 11:48:03 +0200

    First commit, started package for DB Access course

---
+++ src/FirstTopicNameToChange/firstClass.java
@@ -0,0 +1,14 @@
+package FirstTopicNameToChange;
+
+public class firstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello world y todo eso. Esto es una clase para abrir el repo mas facilmente.");
+		
+		
+	}
+
+}
