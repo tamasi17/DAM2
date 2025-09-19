c6003219dc4d2d6ee5d3b0596e40f9152f86351e
Mats <mat.eidelman@gmail.com>
2025-09-19 09:28:31 +0200

    trying out for the first time IntelliJ

---
+++ src/FirstPackage/Main.java
@@ -0,0 +1,20 @@
+package FirstPackage;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    static void main() {
+
+        ArrayList<Integer> lista = new ArrayList<>();
+
+        for (int i = 1; i <= 5; i++) {
+            lista.add(i);
+        }
+
+        for (Integer i : lista) {
+            System.out.println(i);
+        }
+
+        }
+    }
