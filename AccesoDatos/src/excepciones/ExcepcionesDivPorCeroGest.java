8de8e9198620c92866ca92db8b68c102b8db93dd
Mats <mat.eidelman@gmail.com>
2025-09-25 12:10:55 +0200

    ExcepcionesDivPorCeroGest with one try/catch and with three

--- AccesoDatos/src/excepciones/ExcepcionesDivPorCeroGest.java
+++ AccesoDatos/src/excepciones/ExcepcionesDivPorCeroGest.java
@@ -3,14 +3,15 @@ package excepciones;
 // Excepción gestionada durante la ejecución de un programa.
 public class ExcepcionesDivPorCeroGest {
 
-        public int divide(int a, int b) {
-            return a / b;
-        }
+    static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 2;
 
-        public static void main(String[] args) {
-            int a, b;
-            a = 5; b = 2;
+        // Descomentar para probar funcionamiento con tres try-catch
 
+
+        /*
             try {
                 System.out.println(a + " / " + b + " = " + a / b);
             } catch (ArithmeticException e) {
@@ -30,5 +31,23 @@ public class ExcepcionesDivPorCeroGest {
             } catch (ArithmeticException e) {
                 System.err.println("Error al dividir: " + a + " / " + b);
             }
+        */
+
+
+        // Descomentar para probar con un solo try-catch
+        try {
+            System.out.println(a + " / " + b + " = " + a / b);
+            b = 0;
+            System.out.println(a + " / " + b + " = " + a / b);
+            b = 3;
+            System.out.println(a + " / " + b + " = " + a / b);
+        } catch (ArithmeticException e) {
+            System.err.println("Error al dividir: " + a + " / " + b);
         }
+
     }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
