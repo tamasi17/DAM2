37167189eaf73b629ed1d5268e5aa3f6e377003c
Mats <mat.eidelman@gmail.com>
2025-09-25 10:48:35 +0200

    class exercise ExcepcionesConThrows

---
+++ AccesoDatos/src/excepciones/ExcepcionesConThrows.java
@@ -0,0 +1,26 @@
+package excepciones;
+
+// Declaración de excepciones lanzadas por método de clase con throws
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
+public class ExcepcionesConThrows {
+    public File creaFicheroTempConCar(String prefNomFich, char car, int numRep) throws IOException {
+        File f = File.createTempFile(prefNomFich, "");
+        FileWriter fw = new FileWriter(f);
+        for (int i = 0; i < numRep; i++) fw.write(car);
+        fw.close();
+        return f;
+    }
+    public static void main(String[] args) {
+        try {
+            File ft = new ExcepcionesConThrows().creaFicheroTempConCar("AAAA", 'A', 20);
+            System.out.println("Creado fichero: " + ft.getAbsolutePath());
+            ft.delete();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
+
