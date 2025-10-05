f95aea5317abc2589d1e6de9849eb7a9d1402ac7
tamasi17 <mat.eidelman@gmail.com>
2025-10-05 17:58:22 +0200

    finished exercises 8 to 12: kill processes, redirect to file, combine stdout and stderr.

---
+++ ProcesosServicios/src/Ej11_AnalizadorPings.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.classfile.constantpool.Utf8Entry;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class Ej11_AnalizadorPings {
+        /*
+        Tu programa debe capturar la salida estándar (stdout) del proceso línea a línea.
+        Cada vez que una línea indique que el paquete ha sido recibido correctamente
+        (por ejemplo, contiene ttl o bytes), suma un acierto.
+        Si la línea refleja un fallo
+        (por ejemplo, contiene tiempo de espera agotado, timeout o host unreachable), suma un fallo.
+        Al finalizar el proceso, muestra en pantalla un resumen sobre respuestas recibidas y fallos:
+         */
+    static void main() {
+
+        ProcessBuilder pb = new ProcessBuilder("ping", "google.com", "-n", "10");
+        pb.redirectErrorStream(true);
+       try{
+        Process process=pb.start();
+        int aciertos=0;
+        int fallos=0;
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8))){
+            String linea;
+            while ((linea = br.readLine()) != null){
+                System.out.println(linea);
+                if (linea.contains("Respuesta")) {
+                    aciertos++;
+                } else if(linea.contains("solicitud") | linea.contains("Error")) fallos++; // La solicitud de ping no pudo encontrar...
+            }
+       }
+
+           System.out.println("Aciertos: " + aciertos + "\nFallos: " + fallos);
+
+        } catch (IOException e) {
+            System.err.println("Error de E/S");
+        }
+
+    }
+
+}
---
+++ ProcesosServicios/src/Ej8_KillProcess.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
+
+public class Ej8_KillProcess {
+
+    static void main() {
+
+        ProcessBuilder processBuilder = new ProcessBuilder("ping", "google.com", "-n", "30");
+        processBuilder.redirectErrorStream(true);
+
+        try {
+            Process process = processBuilder.start();
+
+            // Esperamos 5 segundos a que el proceso termine.
+            boolean terminado = process.waitFor(5, TimeUnit.SECONDS);
+
+            // Si no ha terminado, lo matamos
+            if (!terminado){
+                System.out.println("Han pasado 5 segundos.\nMatamos proceso.");
+                process.destroyForcibly();
+                process.waitFor();
+
+            } else{ // si ha terminado, mostramos su stdout y su código por pantalla.
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()))){
+                    String linea;
+                    while ((linea=br.readLine()) != null){
+                        System.out.println(linea);
+                    }
+                    int exit = process.exitValue();
+                    System.out.println("Proceso terminado con codigo: " + exit);
+                }
+            }
+        } catch (InterruptedException ie) {
+            System.err.println("El proceso ha sido interrumpido");
+            Thread.currentThread().interrupt(); //flag
+        } catch (
+                IOException ioe) {
+            System.err.println("Error de E/S en la ejecucion o comunicacion del proceso");
+            ioe.printStackTrace();
+        }
+
+    }
+}
