6ca3694806fe6abed4bb7bbfb38da38a77696b35
tamasi17 <mat.eidelman@gmail.com>
2025-10-11 19:18:50 +0200

    (accesoDatos:ficheros) finished Ej4_EscribiendoQuijote, started Ej7_BuscarPalabra

--- AccesoDatos/src/ficheros/Ej4_EscribiendoQuijote.java
+++ AccesoDatos/src/ficheros/Ej4_EscribiendoQuijote.java
@@ -1,7 +1,6 @@
 package ficheros;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Implemente un programa en Java que permita escribir contenido en un fichero de texto y
@@ -21,22 +20,6 @@ public class Ej4_EscribiendoQuijote {
 
     leerQuijote(fichero);
 
-    /*
-    En un lugar de la mancha
-    de cuyo nombre no quiero acordarme,
-    no ha mucho tiempo que vivía un hidalgo
-    de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.
-     */
-
-
-    // escritura fichero
-
-    // lectura fichero
-
-    // manejo excepciones
-
-    // estructura con escribirTexto() y leerTexto()
-
     }
 
     private static void crearFichero(File fichero) {
@@ -48,6 +31,39 @@ public class Ej4_EscribiendoQuijote {
         } catch (IOException ioe) {
             System.out.println("Error al crear el fichero.");
             System.err.println(ioe.getLocalizedMessage());
+        }
+    }
+
+    private static void escribirQuijote(File fichero) {
+        System.out.println("Escribiendo el Quijote en fichero: " + fichero.getName());
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fichero))) {
+            bw.write("En un lugar de la mancha");
+            bw.newLine();
+            bw.write("de cuyo nombre no quiero acordarme");
+            bw.newLine();
+            bw.write("no ha mucho tiempo que vivía un hidalgo");
+            bw.newLine();
+            bw.write("de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.");
+        } catch (FileNotFoundException fnfe){
+            System.err.println("No se encuentra el archivo donde escribir.");
+        } catch (IOException ioe) {
+            System.err.println("Error de E/S" + ioe.getLocalizedMessage());
+            System.exit(1);
+        }
+        System.out.println("Texto escrito correctamente en " + fichero.getPath());
+    }
+
+    private static void leerQuijote(File fichero) {
+        System.out.println("Leyendo Quijote desde fichero: " + fichero.getName());
+        try (BufferedReader br = new BufferedReader(new FileReader(fichero))){
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo" + e.getLocalizedMessage());
+            System.exit(1);
+            // cero si el proceso funciona correctamente. A partir de ahi, nosotros marcamos el exitCode.
         }
     }
 
---
+++ AccesoDatos/src/ficheros/Ej7_BuscarPalabra.java
@@ -0,0 +1,69 @@
+package ficheros;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ej7_BuscarPalabra {
+    /**
+     * Implementa un programa en Java que reciba por línea de comandos el nombre de un fichero de texto y una palabra.
+     * El programa debe leer el fichero y comprobar si esa palabra aparece en él.
+     * Debe mostrar un mensaje indicando si la palabra se ha encontrado o no.
+     * java BuscarPalabra ficheros/documento.txt gato
+     */
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        File fichero = comprobarFichero(sc);
+        String palabraBuscada = comprobarPalabra(sc);
+        buscaPalabra(fichero, palabraBuscada);
+        // DEVUELVE LA LINEA EQUIVOCADA
+
+        sc.close();
+
+    }
+
+    private static String comprobarPalabra(Scanner sc) {
+        System.out.println("Qué palabra buscamos?");
+        String palabraBuscada = sc.next();
+        return palabraBuscada;
+    }
+
+    private static File comprobarFichero(Scanner sc){
+
+        System.out.println("En qué fichero buscamos la palabra?");
+        String nombreFichero = sc.nextLine().trim();
+        File fichero = new File("ficheros\\" + nombreFichero);
+
+        while (!fichero.exists()) {
+            System.out.println("No se ha encontrado el fichero. Escribe otra vez su nombre: ");
+            nombreFichero = sc.nextLine();
+            fichero = new File("/DAM2/ficheros/" + nombreFichero);
+        }
+        return fichero;
+    }
+
+    private static void buscaPalabra(File fichero, String palabraBuscada) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fichero))) {
+            System.out.println("Buscando " + palabraBuscada);
+            String linea;
+            boolean encontrado = false;
+            while ((linea = br.readLine()) != null && !encontrado) {
+                if (linea.contains(palabraBuscada)) {
+                    encontrado = true;
+                }
+            }
+
+            if ((linea = br.readLine()) != null && !encontrado) {
+                System.out.println("No se ha encontrado la palabra " + palabraBuscada);
+            } else {
+                System.out.println("Se ha encontrado la palabra:\n" + linea);
+            }
+        } catch (IOException ioe) {
+            System.err.println("Error de E/S");
+        }
+    }
+}
