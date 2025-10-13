d7eb2df020bf13a61a4abc067ba767603ce2657e
tamasi17 <mat.eidelman@gmail.com>
2025-10-13 14:33:26 +0200

    Refactored all courses to projects: teachers ask for full project as deliverable

---
+++ AccesoDatos_UD1/src/log4Mats/Logger.java
@@ -0,0 +1,27 @@
+package log4Mats;
+
+/**
+ * Clase que define un logger por nivel de prioridad de errores:
+ * Trace, Debug, Info, Warn, Error. (de menor a mayor prioridad)
+ *
+ * Los logs se guardan en .../logs/nombreTienda.log y cuando se llenan se rota fichero.
+ */
+
+
+public class Logger {
+
+    // Crear desde una clase LogManager (un builder) que nos de
+    // private static final Logger logger = LogManager.getLogger(Log4Mats.class);
+    // El Log4Mats.class es el xml
+
+    // error(String error)
+
+    // warn(String warn)
+
+    // info(String info)
+
+    // debug(String debug)
+
+    // trace(String trace)
+
+}
