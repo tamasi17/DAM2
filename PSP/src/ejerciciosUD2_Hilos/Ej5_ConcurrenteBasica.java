dd46d5a29685b98cb968466aa7b24023465abb25
tamasi17 <mat.eidelman@gmail.com>
2025-10-21 15:28:10 +0200

    (psp: u1_Threads) Ej5 added Random and class extends Thread to reuse code

--- PSP/src/ejerciciosUD2_Hilos/Ej5_ConcurrenteBasica.java
+++ PSP/src/ejerciciosUD2_Hilos/Ej5_ConcurrenteBasica.java
@@ -2,7 +2,7 @@ package ejerciciosUD2_Hilos;
 
 import java.util.Random;
 
-public class Ej5_ConcurrenteBasica {
+public class Ej5_ConcurrenteBasica extends Thread {
 
     /*
     Lanza tres hilos distintos, cada uno debe imprimir una letra diferente (A, B y C) diez veces.
@@ -10,19 +10,47 @@ public class Ej5_ConcurrenteBasica {
     Observa cómo se intercalan las salidas en consola
      */
 
+    int waitingTime;
+
+    public Ej5_ConcurrenteBasica(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    public void run() {
+
+        for (int i = 0; i < 5; i++) {
+
+            Random random = new Random();
+            int rand = random.nextInt(3);
+
+            char letra = (char) ('A' + rand);
+
+            String espacio = "";
+            if (rand == 1) {
+                espacio = "-";
+            } else if (rand == 2) {
+                espacio = "---";
+            }
+
+            System.out.println(espacio + letra);
+
+            try {
+                Thread.sleep(waitingTime);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
     static void main() {
 
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                Random random = new Random();
-
-                char letra = 'A' + (char )random.nextInt(2);
-
-                System.out.println("");
-            }
-        })
+        Thread thread1 = new Ej5_ConcurrenteBasica(70);
+        thread1.start();
+        Thread thread2 = new Ej5_ConcurrenteBasica(90);
+        thread2.start();
+        Thread thread3 = new Ej5_ConcurrenteBasica(100);
+        thread3.start();
 
 
     }
