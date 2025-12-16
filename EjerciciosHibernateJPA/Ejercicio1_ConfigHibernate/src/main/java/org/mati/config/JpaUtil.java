6aabaf9d19ebefd3d0a1683eb97f0a16ba96ac7d
tamasi17 <mat.eidelman@gmail.com>
2025-12-16 17:23:07 +0100

    (ej1Hibernate) JpaUtil Entity Manager and Usuario model

--- EjerciciosHibernateJPA/Ejercicio1_ConfigHibernate/src/main/java/org/mati/config/JpaUtil.java
+++ EjerciciosHibernateJPA/Ejercicio1_ConfigHibernate/src/main/java/org/mati/config/JpaUtil.java
@@ -1,6 +1,31 @@
 package org.mati.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class JpaUtil {
+
+    private static final EntityManagerFactory entityManagerFactory = buildManagerFactory();
+
+    private static EntityManagerFactory buildManagerFactory() {
+
+        try {
+            return Persistence.createEntityManagerFactory("musicaPersistencia");
+        } catch (Exception e) {
+            System.err.println("Error al crear Entity Manager" + e.getLocalizedMessage());
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
+        }
+
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory() { return entityManagerFactory; }
+
+    public static void shutdown() {
+        if (entityManagerFactory != null) {
+            entityManagerFactory.close();
+        }
+    }
 
 
 }
--- EjerciciosHibernateJPA/Ejercicio1_ConfigHibernate/src/main/java/org/mati/model/Usuario.java
+++ EjerciciosHibernateJPA/Ejercicio1_ConfigHibernate/src/main/java/org/mati/model/Usuario.java
@@ -1,7 +1,64 @@
 package org.mati.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String nombre;
+
+    private String email;
+
+    private LocalDate fechaRegistro;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String email, LocalDate fechaRegistro) {
+        this.nombre = nombre;
+        this.email = email;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + nombre +
+                ": " + email +
+                "\n" + fechaRegistro;
+    }
 }
